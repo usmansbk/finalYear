fca9f08e8acc9eab4c6041d03f07af01b6a712b0
Usman <usmansbk@gmail.com>
2023-04-27 06:02:55 +0100

    setup firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByd0IvPMbdQEdBsbf9-RPddC5Z7DsNf3A",
+  authDomain: "recipe-khadijah.firebaseapp.com",
+  projectId: "recipe-khadijah",
+  storageBucket: "recipe-khadijah.appspot.com",
+  messagingSenderId: "207822860300",
+  appId: "1:207822860300:web:ee3400c7f6a14153e73e29",
+  measurementId: "G-FNNZVR1VGX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
